d108b0240e0987c8162f4e3695f0728b043251b2
Irina <petitpapillon82@gmail.com>
2022-08-28 23:15:30 +0400

    Add HW_task_plus (r, S, PI, Math.Round, max of all digits in the number)

---
+++ HW_task_plus/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая на вход принимает радиус круга и находит его площадь округленную до целого числа, 
+необходимо вывести максимальную цифру в полученном округлённом значении площади круга
+10 -> 4
+20 -> 7
+30 -> 8*/
+
+Console.Clear();
+Console.WriteLine("Введите радиус круга: ");
+Console.Write("r = ");
+int r = int.Parse(Console.ReadLine());
+//double r = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Найдите площадь круга.");
+double S = Math.PI * Math.Pow(r, 2); //3.14*Math.Pow(r,2);
+//double S1 = Math.Round(S, 2);
+double S1 = Math.Round(S, 0, MidpointRounding.AwayFromZero);
+Console.WriteLine($"Площадь круга, округленная до целого числа, равна {S1}.");
+string str = S1.ToString();
+int max = int.Parse(str[0].ToString());
+for (int i = 1; i < str.Length; i++)
+{
+    if (int.Parse(str[i].ToString()) > max)
+        max = int.Parse(str[i].ToString());
+}
+Console.Write("Максимальная цифра в полученном округлённом значении площади круга равна ");
+Console.WriteLine(max);
